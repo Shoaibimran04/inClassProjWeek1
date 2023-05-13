c8dcf14824329be507a27ad9739958f59e28fda9
shoaib imran <200528971@student.georgianc.on.ca>
2023-05-13 09:50:24 -0400

    week1 in class project

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        carShow carShow = new carShow();
+        carShow car1 = new carShow ("Toyota", "Camry", 2010);
+        carShow  car2 = new carShow ("Honda", "Civic", 2021);
+        carShow  car3 = new carShow ("Ford", "Mustang", 1969);
 
-        System.out.println("car name is" + carShow.Name);
+
+        System.out.println("Car 1: " + car1.toString());
+        System.out.println("Car 2: " + car2.toString());
+        System.out.println("Car 3: " + car3.toString());
     }
 }
--- src/carShow.java
+++ src/carShow.java
@@ -1,5 +1,50 @@
 public class carShow {
+    private String make;
+    private String model;
+    private int year;
 
-    public String Name = "BMW";
-    private double Price = 10000;
+    public carShow(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        if (make == null || make.trim().isEmpty()) {
+            throw new IllegalArgumentException("Make cannot be empty");
+        }
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        if (model == null || model.trim().isEmpty()) {
+            throw new IllegalArgumentException("Model cannot be empty");
+        }
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year < 1900 || year > 2023) {
+            throw new IllegalArgumentException("Year must be between 1900 and 2023");
+        }
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return year + " " + make + " " + model;
+    }
 }
+
